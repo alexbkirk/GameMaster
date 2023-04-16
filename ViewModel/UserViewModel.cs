160b2ea2e14f93f9ca1c2a98779350a21cbdaacc
alexbkirk <alexbkirk@gmail.com>
2023-04-16 18:34:59 -0500

    This PR created the following: - Setup pages for: -- Registration -- Home page - Removed generic Blazor code - Created and setup DB project -Created and setup Data project

---
+++ ViewModel/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameMaster.UI.Models
+{
+    public class UserViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
